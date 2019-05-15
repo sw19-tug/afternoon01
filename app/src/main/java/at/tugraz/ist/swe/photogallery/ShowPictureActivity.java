29d3e542408f081c95afd01ff2d9866e04855df9
Jan Kowalczyk <jan.kowalczyk@student.tugraz.at>
2019-05-15 15:00:31 +0200

    GAL_003 fullscreen view: show/hide system UI on click [KJ,SM]

--- app/src/main/java/at/tugraz/ist/swe/photogallery/ShowPictureActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/photogallery/ShowPictureActivity.java
@@ -3,6 +3,8 @@ package at.tugraz.ist.swe.photogallery;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -14,21 +16,30 @@ import java.io.FileNotFoundException;
 
 public class ShowPictureActivity extends AppCompatActivity {
 
+    boolean isUIhidden;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
-
         setContentView(R.layout.show_picture);
+        hideUI();
+        isUIhidden = true;
 
+        ConstraintLayout cl = findViewById(R.id.fullscreen_layout);
+        cl.setOnClickListener(new ConstraintLayout.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(isUIhidden){
+                    showUI();
+                    isUIhidden = false;
+                } else {
+                    hideUI();
+                    isUIhidden = true;
+                }
+
+            }
+        });
         ImageView iv = findViewById(R.id.fullscreen_picture);
         Intent intent = getIntent();
 
@@ -41,5 +52,24 @@ public class ShowPictureActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    protected void hideUI(){
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+    }
+    protected void showUI(){
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 }
