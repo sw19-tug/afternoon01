752eeca87110ef435e96b36b8e40396420ef7f22
Shofiqur Rahman <rahman@student.tugraz.at>
2019-05-22 15:43:47 +0200

    GAL_004 added share fuctionallity [BC, SR]

--- app/src/main/java/at/tugraz/ist/swe/photogallery/ShowPictureActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/photogallery/ShowPictureActivity.java
@@ -2,42 +2,82 @@ package at.tugraz.ist.swe.photogallery;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
+
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class ShowPictureActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private ShareActionProvider shareActionProvider;
 
-    boolean isUIhidden;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_fullscreen, menu);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menu.findItem(R.id.action_share));
+        setShareIntent(getDefaultShareIntent());
+        return true;
+    }
+
+
+    public void triggerShareAction() {
+        Intent intent = getDefaultShareIntent();
+        if(intent!=null)
+            shareActionProvider.setShareIntent(intent);
+    }
+
+    private Intent getDefaultShareIntent(){
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("image/png");
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.parse(getIntent().getDataString()));
+        return intent;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.action_share:
+                triggerShareAction();
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+    private void setShareIntent(Intent shareIntent) {
+        if (shareActionProvider != null) {
+            shareActionProvider.setShareIntent(shareIntent);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.show_picture);
-        hideUI();
-        isUIhidden = true;
 
-        ConstraintLayout cl = findViewById(R.id.fullscreen_layout);
-        cl.setOnClickListener(new ConstraintLayout.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                if(isUIhidden){
-                    showUI();
-                    isUIhidden = false;
-                } else {
-                    hideUI();
-                    isUIhidden = true;
-                }
+        toolbar = findViewById(R.id.toolbar_fullscreen);
+        toolbar.inflateMenu(R.menu.menu_fullscreen);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-            }
-        });
         ImageView iv = findViewById(R.id.fullscreen_picture);
         Intent intent = getIntent();
 
@@ -48,26 +88,5 @@ public class ShowPictureActivity extends AppCompatActivity {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
-
-    }
-
-    protected void hideUI(){
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                // Hide the nav bar and status bar
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN);
-
-    }
-    protected void showUI(){
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 }
