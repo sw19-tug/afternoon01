7340636c4e3d4e09431939fc01f5a8245d5adacf
Jan Kowalczyk <jan.kowalczyk@student.tugraz.at>
2019-05-22 14:29:58 +0200

    GAL_008 show rotate icons in showpicture if tapped [KJ,SM]

--- app/src/main/java/at/tugraz/ist/swe/photogallery/ShowPictureActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/photogallery/ShowPictureActivity.java
@@ -7,6 +7,7 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -61,6 +62,8 @@ public class ShowPictureActivity extends AppCompatActivity {
                 // Hide the nav bar and status bar
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        LinearLayout ab = findViewById(R.id.layout_action_bar);
+        ab.setVisibility(View.INVISIBLE);
 
     }
     protected void showUI(){
@@ -69,5 +72,7 @@ public class ShowPictureActivity extends AppCompatActivity {
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        LinearLayout ab = findViewById(R.id.layout_action_bar);
+        ab.setVisibility(View.VISIBLE);
     }
 }
