589cdc04ca849e8aa9c17a384948f0abc8a1a784
Katrin Wallner <katrin.wallner@student.tugraz.at>
2019-06-05 16:53:14 +0200

    GAL_007 store image, not working yet [WK,BC]

--- app/src/main/java/at/tugraz/ist/swe/photogallery/MainActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/photogallery/MainActivity.java
@@ -3,7 +3,10 @@ package at.tugraz.ist.swe.photogallery;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.Manifest;
 import android.content.pm.PackageManager;
@@ -23,7 +26,12 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 import at.tugraz.ist.swe.photogallery.adapter.ImageAdapter;
 import at.tugraz.ist.swe.photogallery.adapter.ImageAdapterFactory;
@@ -41,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         // Check Permission for Media
-        if(isReadStorageAllowed()){
+        if (isReadStorageAllowed()) {
             loadImages();
         } else {
             requestStoragePermission();
@@ -52,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final Spinner spinner = findViewById(R.id.spinner_toolBar);
         final ImageAdapter ia = ImageAdapterFactory.generateImageAdapter(this);
-        final Button camera = findViewById(R. id. camera);
+        final Button camera = findViewById(R.id.camera);
         String spinnerValue = spinner.getSelectedItem().toString();
         ia.sortImages(spinnerValue);
 
@@ -90,83 +98,96 @@ public class MainActivity extends AppCompatActivity {
                 Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (camera_intent.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(camera_intent, pic_id);
+
+                    //Create a file to store the image
+                    File photoFile = null;
+                    try {
+                        photoFile = CreateImageFile();
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+                    }
+                }
+                if (photoFile != null) {
+                    Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.provider", photoFile);
+                    camera_intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                            photoURI);
+                    startActivityForResult(camera_intent,
+                            REQUEST_CAPTURE_IMAGE);
                 }
             }
-        });
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
 
-    //We are calling this method to check the permission status
-    private boolean isReadStorageAllowed() {
-        //Getting the permission status
-        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-
-        //If permission is granted returning true
-        if (result == PackageManager.PERMISSION_GRANTED)
-            return true;
-
-        //If permission is not granted returning false
-        return false;
-    }
-
-    //Requesting permission
-    private void requestStoragePermission(){
-
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
-            //If the user has denied the permission previously your code will come to this block
-            //Here you can explain why you need this permission
-            //Explain here why you need this permission
-            //requestStoragePermission();
-
-        }
-
-        //And finally ask for the permission
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},STORAGE_PERMISSION_CODE);
-    }
-
-
-    //This method will be called when the user will tap on allow or deny
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        //Checking the request code of our request
-        if(requestCode == STORAGE_PERMISSION_CODE){
-
-            //If permission is granted
-            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                //Displaying a toast
-                Toast.makeText(this,"Permission granted now you can read the storage",Toast.LENGTH_LONG).show();
-                loadImages();
-            }else{
-                //Displaying another toast if permission is not granted
-                Toast.makeText(this,"Oops you just denied the permission",Toast.LENGTH_LONG).show();
-                requestStoragePermission();
+            @Override
+            public boolean onCreateOptionsMenu(Menu menu) {
+                // Inflate the menu; this adds items to the action bar if it is present.
+                getMenuInflater().inflate(R.menu.menu_main, menu);
+                return true;
             }
+
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+                // Handle action bar item clicks here. The action bar will
+                // automatically handle clicks on the Home/Up button, so long
+                // as you specify a parent activity in AndroidManifest.xml.
+                int id = item.getItemId();
+
+                //noinspection SimplifiableIfStatement
+                if (id == R.id.action_settings) {
+                    return true;
+                }
+
+                return super.onOptionsItemSelected(item);
+            }
+
+            //We are calling this method to check the permission status
+            private boolean isReadStorageAllowed() {
+                //Getting the permission status
+                int result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                //If permission is granted returning true
+                if (result == PackageManager.PERMISSION_GRANTED)
+                    return true;
+
+                //If permission is not granted returning false
+                return false;
+            }
+
+            //Requesting permission
+            private void requestStoragePermission() {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    //If the user has denied the permission previously your code will come to this block
+                    //Here you can explain why you need this permission
+                    //Explain here why you need this permission
+                    //requestStoragePermission();
+
+                }
+
+                //And finally ask for the permission
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+            }
+
+
+            //This method will be called when the user will tap on allow or deny
+            @Override
+            public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+                //Checking the request code of our request
+                if (requestCode == STORAGE_PERMISSION_CODE) {
+
+                    //If permission is granted
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        //Displaying a toast
+                        Toast.makeText(this, "Permission granted now you can read the storage", Toast.LENGTH_LONG).show();
+                        loadImages();
+                    } else {
+                        //Displaying another toast if permission is not granted
+                        Toast.makeText(this, "Oops you just denied the permission", Toast.LENGTH_LONG).show();
+                        requestStoragePermission();
+                    }
+                }
         }
+
     }
-
-
-
 }
